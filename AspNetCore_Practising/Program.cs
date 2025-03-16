8e7c5ea15936c810f85fbdaded36993364b69172
Валерий Кучерявый <kucheryavyi.valeriy@gmail.com>
2025-03-16 18:22:17 +0200

    Add: GET endpoints

--- AspNetCore_Practising/Program.cs
+++ AspNetCore_Practising/Program.cs
@@ -1,6 +1,21 @@
+using AspNetCore_Practising.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+IEnumerable<Todo_Model> todos = new List<Todo_Model>();
 
 app.MapGet("/", () => "Hello World!");
+
+// todos
+app.MapGet("/todos", () => todos);
+app.MapGet("/todos/{id}", Results<Ok<Todo_Model>, NotFound> (int id) =>
+{
+    var record = todos.FirstOrDefault(el => el.Id == id);
+
+    return record == null 
+    ? TypedResults.NotFound()
+    : TypedResults.Ok(record);
+});
 
 app.Run();
