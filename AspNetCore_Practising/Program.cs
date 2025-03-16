5c1636bb83e89e32274b29424f25fcded37cf6c1
Валерий Кучерявый <kucheryavyi.valeriy@gmail.com>
2025-03-16 18:45:09 +0200

    Add: endpoint POST

--- AspNetCore_Practising/Program.cs
+++ AspNetCore_Practising/Program.cs
@@ -8,14 +8,22 @@ var todos = new List<Todo_Model>();
 app.MapGet("/", () => "Hello World!");
 
 // todos
+// get
 app.MapGet("/todos", () => todos);
 app.MapGet("/todos/{id}", Results<Ok<Todo_Model>, NotFound> (int id) =>
 {
     var record = todos.FirstOrDefault(el => el.Id == id);
 
-    return record == null 
+    return record == null
     ? TypedResults.NotFound()
     : TypedResults.Ok(record);
+});
+
+// post
+app.MapPost("/todos", (Todo_Model todo) =>
+{
+    todos.Add(todo);
+    return TypedResults.Created("/todos/{id}", todo);
 });
 
 app.Run();
