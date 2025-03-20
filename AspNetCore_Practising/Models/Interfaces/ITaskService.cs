b376d758a2c1fd7a3463573acce1c019cc4b89e8
Валерий Кучерявый <kucheryavyi.valeriy@gmail.com>
2025-03-20 22:41:12 +0200

    Add: dependency injection

---
+++ AspNetCore_Practising/Models/Interfaces/ITaskService.cs
@@ -0,0 +1,10 @@
+﻿namespace AspNetCore_Practising.Models.Interfaces
+{
+    public interface ITaskService
+    {
+        Todo_Model? GetTodoById(int id);
+        List<Todo_Model> GetTodos();
+        void DeleteTodoById(int id);
+        Todo_Model AddTodo(Todo_Model todo);
+    }
+}
